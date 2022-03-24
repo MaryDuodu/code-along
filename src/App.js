667e6276dfda11ae69558f696976fa2c9cdc5c5e
Mary Duodu <osamduodu57@gmail.com>
2022-03-24 07:51:10 -0700

    React Component 4

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-// import writers from "./writers";
-import {ProfileCard} from "./ProfileCard"; 
+
 import {useEffect, useState} from "react"
+import ProfileCard from "./ProfileCard";
+
 
 function App() {
-  const [writers, setWriters] = useState([]);
+  const [data, setData] = useState({
+    writers: [],
+    loading: false,
+  });
 
-  useEffect(() => {
+const handleClick = () => {
+    setData(prevData => ({
+      ...prevData,
+      loading: true,
+    }));
+  
+  setTimeout(() => {
     const getWriters = async () => {
       const response = await fetch("/writers.json");
       const data = await response.json();
-      setWriters(data);
+      setData({writers: data,
+        loading: false})
     }
     getWriters();
-  }, [])
+  }, 2000);
+};
+
+if (data.loading) {
   return (
-    <div className="App">
-     <h1>Write Profiles</h1>
-     <div className="container">
-        {writers.map((writer) => (
-          <ProfileCard writer={writer} />
-        ))};
-      
+    <div>
+      <h1>Writer Profiles </h1>
+      <div className="container">
+        <div className="card action">
+          <p className="infoText"> loading... </p>
+          </div>
+        </div>
+      </div>
+        );
+      }
+    return (
+      <div>
+      <h1>Writer Profiles </h1>
+      <div className="container">
+        {data.writers.length === 0 ? (
+        <div className="card action">
+          <p className="infoText"> Oops... no witer profile found </p>
+          <button className="actionBtn" onClick={handleClick}>
+            Get writers
+          </button>
+        </div>
+      ) : ( 
+        data.writers.map((writer) =>(
+          <ProfileCard key={writer.id} writer={writer} />
+        ))
+      )}
       </div>
     </div>
-  );
+    );
 }
+
 
 export default App;
