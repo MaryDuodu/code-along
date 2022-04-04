1dbc08a8413173f0a5670eb4e0bd0d9b8a35fb78
Mary Duodu <osamduodu57@gmail.com>
2022-04-04 14:00:23 +0000

    react component 8

---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+        <h3>WELCOME TO THE ABOUT PAGE</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repellat earum libero possimus veniam dignissimos ad beatae dicta tempora placeat, mollitia facilis nemo perspiciatis nulla ipsum quaerat non aut! Nobis.
+        Aperiam officiis voluptate veniam deserunt, mollitia nihil odit dolor necessitatibus quia fugiat eius laudantium sequi corporis animi minima amet dolorem doloremque facere debitis praesentium saepe perferendis asperiores at? Repellendus, iusto!
+        Corrupti tenetur aliquam, sapiente minus exercitationem, enim mollitia in ex aliquid adipisci perspiciatis deleniti cupiditate molestias, consequatur unde ullam eveniet iste? Animi aut nobis quidem culpa placeat est exercitationem non!
+
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repellat earum libero possimus veniam dignissimos ad beatae dicta tempora placeat, mollitia facilis nemo perspiciatis nulla ipsum quaerat non aut! Nobis.
+        Aperiam officiis voluptate veniam deserunt, mollitia nihil odit dolor necessitatibus quia fugiat eius laudantium sequi corporis animi minima amet dolorem doloremque facere debitis praesentium saepe perferendis asperiores at? Repellendus, iusto!
+        Corrupti tenetur aliquam, sapiente minus exercitationem, enim mollitia in ex aliquid adipisci perspiciatis deleniti cupiditate molestias, consequatur unde ullam eveniet iste? Animi aut nobis quidem culpa placeat est exercitationem non!
+
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repellat earum libero possimus veniam dignissimos ad beatae dicta tempora placeat, mollitia facilis nemo perspiciatis nulla ipsum quaerat non aut! Nobis.
+        Aperiam officiis voluptate veniam deserunt, mollitia nihil odit dolor necessitatibus quia fugiat eius laudantium sequi corporis animi minima amet dolorem doloremque facere debitis praesentium saepe perferendis asperiores at? Repellendus, iusto!
+        Corrupti tenetur aliquam, sapiente minus exercitationem, enim mollitia in ex aliquid adipisci perspiciatis deleniti cupiditate molestias, consequatur unde ullam eveniet iste? Animi aut nobis quidem culpa placeat est exercitationem non!
+
+        </p>
+    </section>
+  )
+}
+
+export default About
