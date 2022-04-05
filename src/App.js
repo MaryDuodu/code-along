6dd8ed6866df6e8c5d4dc10dc9e1ee980e2e7b19
Mary Duodu <osamduodu57@gmail.com>
2022-04-05 14:13:19 +0000

    React component 9

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
-import About from "./pages/Home";
+import About from "./pages/About";
 import Navbar from "./components/navbar/Navbar";
+import BlogList from "./pages/BlogList";
+import BlogDetail from "./pages/BlogDetail"
 
 const App = () => {
   return (
@@ -11,7 +13,10 @@ const App = () => {
    <Navbar />
     <Routes>
       <Route path="about" element={<About />} />
-      <Route path="blog" element={<Blog />} />
+      <Route path="blog" element={<Blog />}>
+        <Route path="" element={<BlogList />} />
+        <Route path=":blog" element={<BlogDetail />} /> 
+      </Route>
       <Route path="/" element={<Home />} />
     </Routes>
   
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,26 +1,8 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
 
-const Blog = () => {
-  return (
-    <section>
-        <h3>WELCOME TO THE BLOG PAGE</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repellat earum libero possimus veniam dignissimos ad beatae dicta tempora placeat, mollitia facilis nemo perspiciatis nulla ipsum quaerat non aut! Nobis.
-        Aperiam officiis voluptate veniam deserunt, mollitia nihil odit dolor necessitatibus quia fugiat eius laudantium sequi corporis animi minima amet dolorem doloremque facere debitis praesentium saepe perferendis asperiores at? Repellendus, iusto!
-        Corrupti tenetur aliquam, sapiente minus exercitationem, enim mollitia in ex aliquid adipisci perspiciatis deleniti cupiditate molestias, consequatur unde ullam eveniet iste? Animi aut nobis quidem culpa placeat est exercitationem non!
 
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repellat earum libero possimus veniam dignissimos ad beatae dicta tempora placeat, mollitia facilis nemo perspiciatis nulla ipsum quaerat non aut! Nobis.
-        Aperiam officiis voluptate veniam deserunt, mollitia nihil odit dolor necessitatibus quia fugiat eius laudantium sequi corporis animi minima amet dolorem doloremque facere debitis praesentium saepe perferendis asperiores at? Repellendus, iusto!
-        Corrupti tenetur aliquam, sapiente minus exercitationem, enim mollitia in ex aliquid adipisci perspiciatis deleniti cupiditate molestias, consequatur unde ullam eveniet iste? Animi aut nobis quidem culpa placeat est exercitationem non!
-
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione repellat earum libero possimus veniam dignissimos ad beatae dicta tempora placeat, mollitia facilis nemo perspiciatis nulla ipsum quaerat non aut! Nobis.
-        Aperiam officiis voluptate veniam deserunt, mollitia nihil odit dolor necessitatibus quia fugiat eius laudantium sequi corporis animi minima amet dolorem doloremque facere debitis praesentium saepe perferendis asperiores at? Repellendus, iusto!
-        Corrupti tenetur aliquam, sapiente minus exercitationem, enim mollitia in ex aliquid adipisci perspiciatis deleniti cupiditate molestias, consequatur unde ullam eveniet iste? Animi aut nobis quidem culpa placeat est exercitationem non!
-
-        </p>
-    </section>
-  )
+function Blog() {
+  return <Outlet />;
 }
 
-export default Blog
+export default Blog;
